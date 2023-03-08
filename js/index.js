92b8872bf0e85c3ddf81d4141535d984d876be13
Lsong <song940@gmail.com>
2023-03-08 23:03:10 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-import { ready } from 'https://lsong.org/scripts/dom.js';
-import { h, render, useState, useEffect } from 'https://lsong.org/scripts/components/react.js';
+import { ready, querySelector as $ } from 'https://lsong.org/scripts/dom.js';
 
-const App = () => {
-  const [] = useState();
-  useEffect(() => {
-    console.log('App is ready');
-  }, []);
-  return [
-    h('h2', null, "App"),
-    h('ul', null, [
-      h('li', null, "Demo")
-    ])
-  ]
+function generateBookmarklet(code) {
+  code = `(function(){${code.trim()}})();`;
+  return `javascript:` + encodeURIComponent(code);
 }
 
 ready(() => {
-  const app = document.getElementById('app');
-  render(h(App), app);
+  const $link = $("#bookmarklet");
+  const $name = $("input[name=name]");
+  const $code = $("textarea[name=code]");
+  const $output = $("input[name=output]");
+  const $html = $("input[name=html]");
+  $code.addEventListener("input", e => {
+    e.preventDefault();
+    const name = $name.value;
+    const code = $code.value;
+    const output = $output.value = generateBookmarklet(code);
+    $link.innerHTML = $html.value = `<a href="${output}" >${name}</a>`;
+
+  });
+  $name.addEventListener('input', e => {
+    $link.textContent = e.target.value || 'Bookmarklet Maker';
+  });
 });
